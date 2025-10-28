e605430b1137263f7ef094a3675cbbbf67982916
vinicap1 <viniandrade34@gmail.com>
2025-10-28 02:15:02 +0000

    Tema 3

--- super_trunfo.c
+++ super_trunfo.c
@@ -6,12 +6,13 @@
 
 // Função para calcular Densidade Populacional
 float dens_pop(unsigned long pop, float area) {
-    return pop / area;
+    return (float)pop / area;
 }
 
-// Função para calcular o PIB per capita 
+// Função para calcular o PIB per capita
 float pib_pcapita(unsigned long pib, unsigned long pop) {
-    return pib / pop;
+    //Conversao do PIB de milhoes para reais
+    return (float)((pib*1000000) / pop);
 }
 
 // Função principal
@@ -44,8 +45,12 @@ int main(){
     system("clear||cls");
     printf("\n##### SUPERTRUNFO CIDADES #####\n");
 
+    //Calculando densidade populacional e pib per capita
+    float dens0 = dens_pop(carta_jogador1[i].populacao, carta_jogador1[i].area_km2);
+    float ppc0 = pib_pcapita(carta_jogador1[i].pib_milhoes, carta_jogador1[i].populacao);
+
     //Exibição das cartas
-    printf("*** Cartas Sorteada ***\n");
+    printf("*** Carta Sorteada ***\n");
     i = rand() % 10;
     printf("Código: %s\n", carta_jogador1[i].codigo);
     printf("Estado: %s\n", carta_jogador1[i].estado);
@@ -54,6 +59,8 @@ int main(){
     printf("Área: %.2f km²\n", carta_jogador1[i].area_km2);
     printf("PIB: %lu milhões\n", carta_jogador1[i].pib_milhoes);
     printf("Pontos Turísticos: %d\n", carta_jogador1[i].pontos_turisticos);
+    printf("Densidade Populacional: %.0f hab/km²\n", dens0);
+    printf("PIB per capita: R$%.2f\n", ppc0);
     printf("***********************\n");
     
     
@@ -85,17 +92,79 @@ int main(){
             printf("\nResultado: Jogador2 venceu!!!\n%s - %lu habitantes", carta_jogador2[i].nome, carta_jogador2[i].populacao);
         }
         break;
+
+    case 2:
+        printf("\nÁREA\n");
+        printf("Carta Jogador 1 - Cidade: %s | Área: %.2f\n",carta_jogador1[i].nome, carta_jogador1[i].area_km2);
+        printf("Carta Jogador 2 - Cidade: %s | Área: %.2f\n",carta_jogador2[i].nome, carta_jogador2[i].area_km2);
+
+        if (carta_jogador1[i].area_km2 > carta_jogador2[i].area_km2){
+            printf("\nResultado: Jogador1 venceu!!!\n%s - %.2f km²", carta_jogador1[i].nome, carta_jogador1[i].area_km2); 
+        } else {
+            printf("\nResultado: Jogador2 venceu!!!\n%s - %.2f km²", carta_jogador2[i].nome, carta_jogador2[i].area_km2);
+        }
+        break;
+    
+    case 3:
+        printf("\nPIB\n");
+        printf("Carta Jogador 1 - Cidade: %s | Área: %lu\n",carta_jogador1[i].nome, carta_jogador1[i].pib_milhoes);
+        printf("Carta Jogador 2 - Cidade: %s | Área: %lu\n",carta_jogador2[i].nome, carta_jogador2[i].pib_milhoes);
+
+        if (carta_jogador1[i].pib_milhoes > carta_jogador2[i].pib_milhoes){
+            printf("\nResultado: Jogador1 venceu!!!\n%s - PIB %lu milhões de reais", carta_jogador1[i].nome, carta_jogador1[i].pib_milhoes); 
+        } else {
+            printf("\nResultado: Jogador2 venceu!!!\n%s - PIB %lu milhões de reais", carta_jogador2[i].nome, carta_jogador2[i].pib_milhoes);
+        }
+        break;
+    
+    case 4:
+        printf("\nNúmero de Pontos Turísticos\n");
+        printf("Carta Jogador 1 - Cidade: %s | Número de Pontos Turísticos: %d\n",carta_jogador1[i].nome, carta_jogador1[i].pontos_turisticos);
+        printf("Carta Jogador 2 - Cidade: %s | Número de Pontos Turísticos: %d\n",carta_jogador2[i].nome, carta_jogador2[i].pontos_turisticos);
+
+        if (carta_jogador1[i].pontos_turisticos > carta_jogador2[i].pontos_turisticos){
+            printf("\nResultado: Jogador1 venceu!!!\n%s - %d Pontos Turísticos", carta_jogador1[i].nome, carta_jogador1[i].pontos_turisticos); 
+        } else {
+            printf("\nResultado: Jogador2 venceu!!!\n%s - %d Pontos Turísticos", carta_jogador2[i].nome, carta_jogador2[i].pontos_turisticos);
+        }
+        break;
+
+    case 5:
+        float dens1, dens2;
+        dens1 = dens_pop(carta_jogador1[i].populacao, carta_jogador1[i].area_km2);
+        dens2 = dens_pop(carta_jogador2[i].populacao, carta_jogador2[i].area_km2);    
+        
+        printf("\nDensidade Populacional\n");
+        printf("Carta Jogador 1 - Cidade: %s | Densidade Populacional: %.2f Habitantes por km²\n",carta_jogador1[i].nome, dens1);
+        printf("Carta Jogador 2 - Cidade: %s | Densidade Populacional: %.2f Habitantes por km²\n",carta_jogador2[i].nome, dens2);
+
+        if (dens1 < dens2){
+            printf("\nResultado: Jogador1 venceu!!!\n%s - %.2f hab/km²", carta_jogador1[i].nome, dens1); 
+        } else {
+            printf("\nResultado: Jogador2 venceu!!!\n%s - %.2f hab/km²", carta_jogador2[i].nome, dens2);
+        }
+        break;
+
+    case 6:
+        float ppc1, ppc2;
+        ppc1 = pib_pcapita(carta_jogador1[i].pib_milhoes, carta_jogador1[i].populacao);
+        ppc2 = pib_pcapita(carta_jogador2[i].pib_milhoes, carta_jogador2[i].populacao);    
+        
+        printf("\nPIB Per Capita\n");
+        printf("Carta Jogador 1 - Cidade: %s | PIB Per Capita: R$%.2f \n",carta_jogador1[i].nome, ppc1);
+        printf("Carta Jogador 2 - Cidade: %s | PIB Per Capita: R$%.2f \n",carta_jogador2[i].nome, ppc2);
+
+        if (ppc1 > ppc2){
+            printf("\nResultado: Jogador1 venceu!!!\n%s - PIB per capita: R$%.2f", carta_jogador1[i].nome, ppc1); 
+        } else {
+            printf("\nResultado: Jogador2 venceu!!!\n%s - PIB per capita: R$%.2f", carta_jogador2[i].nome, ppc2);
+        }
+        break;
     
     default:
         break;
     }
 
-    //Comparacao de atributos
-    //if (c1.area > c2.area){
-    //    printf("\nResultado: Carta 1 (%s) venceu!", c1.nomeDaCidade); 
-    //} else {
-    //    printf("\nResultado: Carta 2 (%s) venceu!", c2.nomeDaCidade);
-    //}
     getchar();
     printf("\n\nDigite ENTER para encerrar...");
     getchar();
